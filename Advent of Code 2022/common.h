2f982d8193e13c11c895808cb72fbc01356bca62
ehenry344 <ehenry344@gmail.com>
2022-12-07 13:01:32 -0500

    Added common header

---
+++ Advent of Code 2022/common.h
@@ -0,0 +1,20 @@
+#ifndef _Advent2022_EvanHenry 
+#define _Advent2022_EvanHenry
+
+// Common Include Paths 
+
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <numeric> 
+#include <regex> 
+
+// .h c style libraries 
+
+#include <math.h>  
+
+
+
+#endif
